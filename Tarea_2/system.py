1a1ae3fc7c7305b2902a0ad5109b73831886543e
Jorge Juarez <jorgeang33@gmail.com>
2023-02-26 23:27:41 -0600

    Add variables

--- Tarea_2/system.py
+++ Tarea_2/system.py
@@ -7,7 +7,14 @@ ruta_archivo = os.path.join('Tarea_2', 'autobuses2.xml')
 tree = ET.parse(ruta_archivo)
 root = tree.getroot()
 
+# Variables del Script para la imprecion en consola, y el cambio de valores del archivo .xml
 i = 0
+# Catalogo Zona
+one = "Sur"
+two = "Norte"
+three = "Este"
+four = "Oeste"
+five = "Centro"
 
 for autobus in root:
     placa = autobus.find('placa').text
