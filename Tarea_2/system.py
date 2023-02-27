03dea80c0b3b5c68b62d6cb3810d27dc3033890f
Jorge Juarez <jorgeang33@gmail.com>
2023-02-27 00:41:19 -0600

    Lectura de .txt con system.py

--- Tarea_2/system.py
+++ Tarea_2/system.py
@@ -2,15 +2,25 @@ import os
 import xml.etree.ElementTree as ET
 
 # Obtiene la ruta de de la carpeta con el archivo cascade.xml
-ruta_archivo = os.path.join('Tarea_2', 'autobuses2.xml')
+ruta_xml = os.path.join('Tarea_2', 'autobuses2.xml')
+ruta_txt = os.path.join('Tarea_2', 'secciones.txt')
 
-tree = ET.parse(ruta_archivo)
+tree = ET.parse(ruta_xml)
 root = tree.getroot()
 
 # Variables del Script para la imprecion en consola, y el cambio de valores del archivo .xml
 i = 0
 # Catalogo Zona
 zonas = ["Sur", "Norte", "Este", "Oeste", "Centro"]
+
+# Abre el archivo de texto donde vienen las secciones en modo lectura
+with open(ruta_txt, 'r') as archivo:
+    # Lee todas las líneas del archivo y las guarda en una lista
+    lista_secciones = archivo.readlines()
+
+# Imprime la lista con el contenido del archivo
+print(lista_secciones)
+
 
 for autobus in root:
     placa = autobus.find('placa').text
