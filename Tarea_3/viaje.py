706f764989674c96f769bf1b83853b393260bc4a
jorge Juarez <jorgeang33@gmail.com>
2023-05-27 22:42:46 -0600

    Verificar el tipo de inputs por el tipo y caracteristica del dato

--- Tarea_3/viaje.py
+++ Tarea_3/viaje.py
@@ -1,4 +1,5 @@
 from num2words import num2words
+import time
 
 
 class Viaje:
@@ -12,12 +13,13 @@ class Viaje:
 
     def __str__(self):
         cadena = f"""
-Clave:  {self.__clave}
-Destino:  {self.__destino}
-Autobus:  {self.__autobus}
-Precio:  $ {self.__precio} MXN ({num2words(self.__precio, lang="es").upper()})
+Clave:  {self.__clave.upper()}
+Destino:  {self.__destino.capitalize()}
+Placa del autobus:  {self.__autobus.upper()}
+Precio:  $ {self.__precio} MXN ({num2words(self.__precio, lang="es").upper()} PESOS)
 Hora de salida:  {self.__hora} hrs
 Puerta:  {self.__puerta}"""
+
         return cadena
 
 
@@ -27,16 +29,67 @@ def insertarViaje():
 
     print("Proporciona los siguinetes datos:")
     for item in datos:
-        if item == 'hora':
-            print("\nHora de salida:")
+        # Aclaraciones al usuario sobre la informacion que se le pide
+        if item == 'autobus':
+            print("\nPlaca del autobus: ")
+        elif item == 'hora':
+            print("\nHora de salida (24 hrs):")
         else:
             print(f"\n{item.capitalize()}: ")
-        valor = input("  > ")
-        datos[item] = valor
+
+        # Verificaciones de la informacion ingresada por el usuario
+        if item == 'destino':
+            while True:
+                valor = input("  > ")
+                if valor.isalpha():
+                    datos[item] = valor
+                    break
+                else:
+                    print(" ** Ingresa un destino valido **", end="\r")
+                    time.sleep(1)
+                    print(" " * 35, end="\r")
+
+        elif item == 'precio':
+            while True:
+                valor = input("  > $ ")
+                if valor.isdigit():
+                    datos[item] = valor
+                    break
+                else:
+                    print(" ** Ingresa un precio valido **", end="\r")
+                    time.sleep(1)
+                    print(" " * 35, end="\r")
+
+        elif item == 'hora':
+            while True:
+                valor = input("  > ")
+                if valor.isdigit() and int(valor) in range(0, 24):
+                    datos[item] = valor
+                    break
+                else:
+                    print(" ** Ingresa una hora valida **", end="\r")
+                    time.sleep(1)
+                    print(" " * 35, end="\r")
+
+        elif item == 'puerta':
+            while True:
+                valor = input("  > ")
+                if valor.isdigit():
+                    datos[item] = valor
+                    break
+                else:
+                    print(" ** Ingresa un numero de puerta valido **", end="\r")
+                    time.sleep(1)
+                    print(" " * 42, end="\r")
+
+        else:
+            valor = input("  > ")
+            datos[item] = valor
 
     viaje = Viaje(**datos)
+
     return viaje
 
 
-viaje = insertarViaje()
-print(viaje)
+viaje2 = insertarViaje()
+print(viaje2)
