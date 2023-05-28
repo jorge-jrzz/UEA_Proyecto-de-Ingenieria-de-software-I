f3674816b6f37b808440ccecee46b7a620d952aa
jorge Juarez <jorgeang33@gmail.com>
2023-05-27 22:51:37 -0600

    documentando el codigo

--- Tarea_3/app.py
+++ Tarea_3/app.py
@@ -2,21 +2,23 @@ import os
 import time
 
 
-def clearScreen():
+def clear_screen():
+    """Funcion para limpiar la pantalla de la terminal"""
     if os.name == "posix" or os.name == "mac":
         os.system("clear")
     elif os.name == "ce" or os.name == "nt" or os.name == "dos":
         os.system("cls")
 
 
-def verificar(opcion):
-    for c in opcion:
+def verificar(option):
+    """Funcion para verificar la opcion del usuario que este dentro de las opciones establecidas"""
+    for c in option:
         if c not in 'abcde':
             return False
         else:
             return True
 
-    for i in opcion:
+    for i in option:
         if i not in '12345':
             return False
         else:
@@ -24,7 +26,7 @@ def verificar(opcion):
 
 
 while True:
-    clearScreen()
+    clear_screen()
     print("**********************************")
     print("****** Central de Autobuses ******")
     print("**********************************\n")
