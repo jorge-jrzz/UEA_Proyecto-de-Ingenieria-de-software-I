931ccca7c564de672098ba927b29e04c5cbe23cf
jorge Juarez <jorgeang33@gmail.com>
2023-05-27 23:45:30 -0600

    Añadiendo la opcion de salir y poder agregar multiples viajes

--- Tarea_3/app.py
+++ Tarea_3/app.py
@@ -1,5 +1,6 @@
 import os
 import time
+from viaje import insertar_viaje
 
 
 def clear_screen():
@@ -12,17 +13,18 @@ def clear_screen():
 
 def verificar(option):
     """Funcion para verificar la opcion del usuario que este dentro de las opciones establecidas"""
-    for c in option:
-        if c not in 'abcde':
-            return False
-        else:
-            return True
-
-    for i in option:
-        if i not in '12345':
-            return False
-        else:
-            return True
+    if option.isalpha():
+        for c in option:
+            if c not in 'abcdef':
+                return False
+            else:
+                return True
+    else:
+        for i in option:
+            if i not in '123456':
+                return False
+            else:
+                return True
 
 
 while True:
@@ -34,15 +36,46 @@ while True:
     print("b) Mostrar viajes")
     print("c) Buscar viajes")
     print("d) Eliminar viaje")
-    print("e) Actualizar viaje \n")
+    print("e) Actualizar viaje")
+    print("f) Salir \n")
 
-    opcion = input("Selecciona la opción:  ")
+    opcion = input("Selecciona la opción (a-f) or (1-6):  ")
     opcion = opcion.lower()
 
     if verificar(opcion):
+        if opcion == 'f' or opcion == '6':
+            clear_screen()
+            print("\n * VUELVA PRONTO *")
+            exit()
         break
     else:
         print("\n** Ingresa una opcion valida **")
         time.sleep(1)
 
-print(opcion)
+viajes = []
+
+if opcion == 'a' or opcion == '1':
+    while True:
+        clear_screen()
+        viaje = insertar_viaje()
+        viajes.append(viaje)
+        agregar = input("\n ¿Deseas ingresar otro viaje? (S/N)  ")
+        agregar = agregar.lower()
+
+        if agregar not in 'sn':
+            while True:
+                print("** Ingresa una opcion valida **", end="\r")
+                time.sleep(1)
+                print(" " * 40)
+                agregar = input(" ¿Deseas ingresar otro viaje? (S/N)  ")
+                if agregar.lower() == 'n' or agregar.lower() == 's':
+                    break
+
+        if agregar.lower() == 'n':
+            break
+        elif agregar.lower() == 's':
+            pass
+
+elif opcion == 'b' or opcion == '2':
+    clear_screen()
+    print(viajes)
